d61fbdaded856dd13de8444cd12d4aea17283802
Razzh <136013779@qq.com>
2021-10-31 21:55:47 +0800

    add:modules test

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+// const files = require.context('./modules', false, /\.js$/);
+// console.dir(files);
 
-Vue.use(Vuex)
+// files.keys().forEach(key => {
+//   // if(key === './index.js') return;
+//   // console.log(files(key).default)
+//   // console.log(8,files(key))
+//   // configRouters = configRouters.concat(files(key).default)
+// })
+// // console.log(configRouters)
+// // console.log(11,files.keys())
+// const obj = files(files.keys()[1]);
+// console.log(12,obj)
+// // console.log(13,files.keys()[1])
+// const cache = {};
+const path = require('path')
+const files = require.context('./modules', false, /\.js$/)
+let modules = {};
+// function importAll(r) {
+//   r.keys().forEach((key) => (cache[key] = r(key)));
+// }
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+// importAll(require.context('./modules', false, /\.js$/));
+files.keys().forEach(key => {
+  let name = path.basename(key, '.js')
+  modules[name] = files(key).default || files(key)
 })
+console.log(28,files(files.keys()[0]))
+console.log(29,files(files.keys()[0]).default)
+console.log(modules)
---
+++ src/store/modules/test1.js
@@ -0,0 +1,13 @@
+console.log('test1');
+const obj = {
+  name: 'razzh',
+  age: 18
+}
+const obj1 = {
+  name: 'ff',
+  age: 20
+}
+export default {
+  obj,
+  obj1
+};
