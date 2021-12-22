f9dc7a81619ef6f58618d00e4557489725be5afb
Razzh <136013779@qq.com>
2021-12-22 10:09:53 +0800

    add:样式调整

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -2,14 +2,18 @@
   <div class="logo" v-show="true">
     <div class="img-container">
       <img src="../../../assets/logo/logo.png" alt="Vue-admin">
-      <span class="logo-name">Vue-admin-razzh</span>
+      <span class="logo-title">{{ logoTitle }}</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      logoTitle: 'Vue Admin WebApp'
+    }
+  }
 }
 </script>
 
@@ -28,11 +32,11 @@ export default {
       height: 100%;
       vertical-align: middle;
     }
-    .logo-name {
+    .logo-title {
       display: inline-block;
       margin: 0;
       color: #fff;
-      font-weight: 600;
+      font-weight: 700;
       line-height: 50px;
       font-size: 14px;
       font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
