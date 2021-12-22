2586b2877712bbf3d8165806e867b942569c0a0a
Razzh <136013779@qq.com>
2021-12-22 11:51:39 +0800

    update:vue config

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 const resolve = function(dir) {
   return path.join(__dirname, dir);
 }
-console.log('config',process.env.VUE_APP_BASE_API)
 module.exports = {
+  publicPath: '/',
   chainWebpack: config => {
     config.resolve.alias
       .set('@', resolve('src'))
