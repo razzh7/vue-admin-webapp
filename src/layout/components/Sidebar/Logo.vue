6e8f08e6f9f5e4071bc716eab7b4432d69d7a6ce
Razzh <136013779@qq.com>
2022-06-15 17:39:58 +0800

    feat:logo切换新增动画效果

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,40 +1,68 @@
 <template>
   <div class="logo" v-show="true">
-    <div class="img-container">
-      <img src="../../../assets/logo/logo.png" alt="Vue-admin">
+  <transition name="sidebarLogoFade">
+    <router-link  v-if="collapse" key="collapse" class="img-container" to="/">
+      <img :src="logo" alt="Vue-admin">
       <span class="logo-title">{{ logoTitle }}</span>
-    </div>
+    </router-link>
+    <router-link  v-if="!collapse" key="expend" class="img-container" to="/">
+      <img :src="logo" alt="Vue-admin">
+      <span class="logo-title">{{ logoTitle }}</span>
+    </router-link>
+  </transition>
   </div>
 </template>
 
 <script>
+import logo from '../../../assets/logo/logo.png'
 export default {
   data() {
     return {
-      logoTitle: 'Vue Admin WebApp'
+      logoTitle: 'Vue Admin WebApp',
+      logo: logo
     }
-  }
+  },
+  props: {
+    collapse: {
+      type: Boolean,
+      default: true,
+    },
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+.sidebarLogoFade-enter-active {
+  transition: opacity 1.5s;
+}
+
+.sidebarLogoFade-enter,
+.sidebarLogoFade-leave-to {
+  opacity: 0;
+}
+
 .logo {
   width: 100%;
   height: 50px;
   line-height: 50px;
   text-align: center;
-  border-bottom: 1px dashed #ccc;
-  background-color: #021423;
+  // border-bottom: 1px dashed #ccc;
+  background-color: #2b2f3a;
   overflow: hidden;
   .img-container {
-    height: 30px;
+    width: 100%;
+    height: 100%;
+
     img {
-      height: 100%;
+      width: 32px;
+      height: 32px;
       vertical-align: middle;
     }
+
     .logo-title {
       display: inline-block;
-      margin: 0;
+      // margin: 0;
+      margin-left: 10px;
       color: #fff;
       font-weight: 700;
       line-height: 50px;
