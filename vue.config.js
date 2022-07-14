375617fd43c0f8a617cbb0c2f0fd2e432f44a6e1
Razzh <136013779@qq.com>
2022-07-14 23:27:21 +0800

    chore: svg icon

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,30 @@
 const path = require('path');
 const resolve = function(dir) {
   return path.join(__dirname, dir);
-}
+};
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-admin-webapp/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/vue-admin-webapp/' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  chainWebpack: config => {
+  chainWebpack: (config) => {
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end();
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]',
+      })
+      .end();
     config.resolve.alias
       .set('@', resolve('src'))
-      .set('views', resolve('src/views'))
+      .set('views', resolve('src/views'));
   },
-}
+};
