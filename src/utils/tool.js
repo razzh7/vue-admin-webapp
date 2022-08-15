6947b98b892c98aadb7f8a6afb2cb2bf60c376e6
Razzh <136013779@qq.com>
2022-08-15 13:39:36 +0800

    feat: 新增el comfirmBox二次封装

--- src/utils/tool.js
+++ src/utils/tool.js
@@ -1,3 +1,5 @@
+import { Message, MessageBox } from 'element-ui'
+
 export const matchMap = (mapArr, val, isMapIcon = false) => {
   const mapItem = mapArr.find(item => item.dictValue == val)
   if (isMapIcon) {
@@ -17,4 +19,36 @@ export const matchMap = (mapArr, val, isMapIcon = false) => {
   } else {
     return ''
   }
+}
+
+/**
+ * @param {stirng} tips
+ * @param {string} title
+ * @param {string} type
+ * @param {function} fn
+*/
+export function comfirmBox(tips, title, type, fn) {
+  if (!tips) tips = '此操作将永久删除该条数据, 是否继续?'
+  if (!title) title = '提示'
+  if (!type) type = 'warning'
+  MessageBox.confirm(tips, title, {
+    confirmButtonText: '确定',
+    cancelButtonText: '取消',
+    type,
+  })
+    .then(async () => {
+      const { msg } = await fn()
+      Message({
+        showClose: true,
+        type: 'success',
+        message: msg,
+      })
+    })
+    .catch(err => {
+      Message({
+        showClose: true,
+        type: 'info',
+        message: err === 'cancel' ? '取消删除' : err,
+      })
+    })
 }
